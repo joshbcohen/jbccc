2745a972ed5ad8a725f258de96348ab40072b5fd
Joshua Cohen <jbcohen42@gmail.com>
2026-04-14 18:40:07 -0400

    Add barebones CLI

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use clap::Parser;
+
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    c_filepath: String,
+    #[arg(short, long)]
+    lex: bool,
+    #[arg(short, long)]
+    parse: bool,
+    #[arg(short, long)]
+    codegen: bool,
+    #[arg(short='S', long)]
+    emit_assembly: bool,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    println!("path: {:?}", cli.c_filepath);
+    println!("lex: {:?}", cli.lex);
+    println!("emit: {:?}", cli.emit_assembly);
 }
